e809f39209dc1624c9f8f9d41c0471fadb25be77
shubham <shubhamanand733@gmail.com>
2022-07-17 10:10:11 +0530

    Again commit

--- app.js
+++ app.js
@@ -53,7 +53,11 @@ app.get("/signup", function(req, res){
 
 
 if(process.env.NODE_ENV == "production"){
-    app.use(express.static("client/build"))
+    app.use(express.static("client/build"));
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
 }
 
 
